eee9b5ba803f2b9faadaa446be80c35d86767747
Shayxon <shayxon1@gmail.com>
2024-02-19 23:07:06 +0500

    change templates and adding urls for about and blog_post pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
+    path('blog-posts/', views.blog_posts, name='blog_posts'),
+    path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'blog/index.html')
+
+def blog_posts(request):
+    return render(request, 'blog/blog-post.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
