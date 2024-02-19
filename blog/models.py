fe02e382f3972ee0057f8114257f79484eb7e706
Shayxon <shayxon1@gmail.com>
2024-02-19 23:28:43 +0500

    create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_ckeditor_5.fields import CKEditor5Field
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250, blank=False)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    image = models.ImageField(upload_to='media/%Y/%m/%d/', blank=False)
+    body = CKEditor5Field(blank=False)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self) -> str:
+        return self.title    
+
